26e1e5eb187a74850b38300b8941e6d0d599b40f
EvoNick-IR <mostafa.haghani69@Gmail.com>
2023-10-30 20:52:32 +0330

    create some folders - controller , model , router . and fix homePage route

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT;
 app.listen(port, () => console.log(`app is starts on port: ${port}`));
+
+const homePage = require("./routes/home");
+
+app.use("/", homePage);
